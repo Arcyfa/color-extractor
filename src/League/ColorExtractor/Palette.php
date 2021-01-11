3cb1484cea71af9f24b0b7d1158ce8a03064544c
Maarten <Arcyfa@users.noreply.github.com>
2021-01-11 16:56:46 +0100

    Added check for instanceof GdImage
    To support php 8.0 we check if $image is a instance of class GdImage instead of a resource.

--- src/League/ColorExtractor/Palette.php
+++ src/League/ColorExtractor/Palette.php
@@ -68,7 +68,9 @@ class Palette implements \Countable, \IteratorAggregate
      */
     public static function fromGD($image, $backgroundColor = null)
     {
-        if (!is_resource($image) || get_resource_type($image) != 'gd') {
+        if($image instanceof \GdImage){
+            // ... al is ok on php 8
+        }elseif (!is_resource($image) || get_resource_type($image) != 'gd') {
             throw new \InvalidArgumentException('Image must be a gd resource');
         }
         if ($backgroundColor !== null && (!is_numeric($backgroundColor) || $backgroundColor < 0 || $backgroundColor > 16777215)) {
